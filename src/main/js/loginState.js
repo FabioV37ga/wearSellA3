6143bc14b86246e7b7c59833fbea766c499f0de8
Fábio Veiga <veigabfabio@hotmail.com>
2022-05-31 17:27:33 -0300

    31.05 - Auth fields

---
+++ src/main/js/loginState.js
@@ -0,0 +1,21 @@
+/*
+    Esse arquivo é responsável por identificar o estado de login, e redirecionar
+    o usuário se necessário.
+*/
+
+
+
+// Variavel anexado na window, responsável por identificar se o usuário está autenticado
+// 0 = desconectado;
+// 1 = conectado;
+window.LOGIN_STATE = 0;
+
+// Essa IIFE é responsavel por redirecionar o usuário para a tela de login,
+// caso ainda não esteja autenticado e tente acessar o perfil.
+// IIFE checkAndRedirect()
+(function () {
+    if (window.LOGIN_STATE == 0) {
+        console.log("Desconectado, redirecionando...")
+        window.open("login.html", "_self")
+    }
+})()
