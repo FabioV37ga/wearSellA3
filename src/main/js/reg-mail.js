3749c2b0cb30933e1fb33de59769a3f364217f72
Fábio Veiga <veigabfabio@hotmail.com>
2022-06-02 14:23:50 -0300

    02.06 - Campo CPF, formatação e consistencia

---
+++ src/main/js/reg-mail.js
@@ -0,0 +1,85 @@
+/*
+    Esse arquivo é responsável por fazer consistência de alguns campos e
+    manter informações entre telas diferentes.
+
+    Indice:
+        1 - IIFE formatCPF()
+        2 - IIFE reformatCPF()
+        3 - function TestaCPF()
+*/
+
+
+
+// [Element / HTML] Input do CPF (registro)
+const cpfField = document.querySelector(".register-mail").children[1];
+
+// Essa IIFE é responsavel por formatar o display do CPF na tela, adicionando '.' e '-'
+// 1 -- IIFE formatCPF() -- 1
+(function () {
+
+    // Adiciona listener keypress
+    cpfField.addEventListener("keypress", () => {
+        var cpfLeng = cpfField.value.length
+
+        // troca os valores nos caracteres respectivos por . e -
+        if (cpfLeng == 3 || cpfLeng == 7) {
+            cpfField.value = cpfField.value + "."
+        } else if (cpfLeng == 11) {
+            cpfField.value = cpfField.value + "-"
+        }
+    })
+})();
+
+
+
+// Essa IIFE é responsavel por remover quaisquer caracteres que atrapalhem na verificação do cpf
+// 2 -- IIFE reformatCPF() -- 2
+(function () {
+    // Botão final, commit.
+    const registerButton = document.querySelectorAll(".commit-button")[1];
+
+    // Adiciona listener.click no botao.
+    registerButton.addEventListener("click", regCommit);
+
+    // Essa sub-funcao remove espaços, pontos e hifens
+    function regCommit() {
+        var field = cpfField.value.replaceAll(" ", "");
+        field = field.replaceAll(".", "");
+        field = field.replaceAll("-", "");
+
+        console.log("CPF: " + field)
+        
+        // esse log retorna se o cpf é valido ou não.
+        if (TestaCPF(field) == true) {
+            console.log("ver")
+        } else {
+            console.log("fal")
+        }
+    }
+
+})();
+
+
+
+// Essa função é responsável por verificar se o CPF inserido é válido, função oficial do site da receita federal.
+// 3 -- function TestaCPF -- 3
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+}
