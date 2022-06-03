77edd5179ff417262e120ce17ab8ef742b9804db
Fábio Veiga <veigabfabio@hotmail.com>
2022-06-03 16:50:22 -0300

    03.06 - image without css

--- src/main/js/cpf-pass-fill.js
+++ src/main/js/cpf-pass-fill.js
@@ -9,6 +9,17 @@
     console.log(localStorage.getItem("cpf"))
     console.log(localStorage.getItem("pass"))
 
+    const cpfField = ["cpf", document.querySelector(".register-CPF").children[1]]
+    const passField = ["pass", document.querySelector(".register-pass").children[1]]
+
+    var fields = [cpfField, passField]
+
+    for (let i = 0; i <= fields.length - 1; i++) {
+        fields[i][1].value = localStorage.getItem(fields[i][0])
+    }
+
+    localStorage.setItem("cpf","")
+    localStorage.setItem("pass","")
 })()
 
 
