417a6aea06e09776130a96fa2628a3e55204029d
Fábio Balestrin Veiga - 822129856 <822129856@ulife.com.br>
2022-06-03 21:32:29 -0300

    03.06 - PSC Consistencia de campos de registro

---
+++ src/main/js/register-control.js
@@ -0,0 +1,121 @@
+/*
+    target: registrar.html
+
+    Esse arquivo é responsável por preencher automaticamente os campos de CPF e SENHA
+    com so respectivos valores dos campos em 'login.html'
+*/
+
+
+
+(function () {
+    // console.log(localStorage.getItem("cpf"))
+    // console.log(localStorage.getItem("pass"))
+
+    const cpfField = ["cpf", document.querySelector(".register-CPF").children[1]]
+    const passField = ["pass", document.querySelector(".register-pass").children[1]]
+
+    var fields = [cpfField, passField]
+
+    for (let i = 0; i <= fields.length - 1; i++) {
+        fields[i][1].value = localStorage.getItem(fields[i][0])
+    }
+
+    localStorage.setItem("cpf", "")
+    localStorage.setItem("pass", "")
+})();
+
+
+
+
+(function () {
+    const button = document.querySelector(".commit-button");
+
+
+    const campoMail = document.querySelector(".register-mail").children[1];
+
+
+     
+
+    button.addEventListener("click", commit);
+    function commit() {
+        var passValid = 0;
+        var mailValid = 0;
+        var cpfValid = 0;
+        console.log("click")
+        var texto = campoMail.value
+        // console.log(texto)
+
+        var texto_split = texto.split("");
+
+        // CONSISTENCIA EMAIL
+
+        var mailAmount = 0;
+        for (let i = 0; i <= texto_split.length - 1; i++) {
+            if (texto_split[i] == "@") {
+                if (i > 3) {
+                    mailValid = 1;
+                    mailAmount++;
+                    if (mailAmount > 1) {
+                        mailValid = 0;
+                        console.log("multiple")
+                        break;
+                    }
+                }
+            } else {
+                
+            }
+        }
+
+        if (mailValid == 1) {
+            console.log("Email valido.")
+           
+        } else {
+            
+        }
+
+        // CONSISTENCIA CPF
+        
+        
+        function TestaCPF(strCPF) {
+            var Soma;
+            var Resto;
+            Soma = 0;
+            if (strCPF == "00000000000") return false;
+            
+            for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+            Resto = (Soma * 10) % 11;
+            
+            if ((Resto == 10) || (Resto == 11)) Resto = 0;
+            if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+            
+            Soma = 0;
+            for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+            Resto = (Soma * 10) % 11;
+            
+            if ((Resto == 10) || (Resto == 11)) Resto = 0;
+            if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+            return true;
+        }
+        if(TestaCPF(document.querySelector(".register-CPF").children[1].value) == true){
+            console.log("cpf valido")
+            cpfValid = 1;
+        }
+
+        // Consistencia SENHA
+
+       
+        const passwords = document.querySelectorAll(".register-pass")
+
+        if (passwords[0].children[1].value == passwords[1].children[1].value && passwords[0].children[1].value != ""){
+            if (passwords[0].children[1].value.length > 7){
+                passValid = 1;
+            }   
+        }
+
+        if (passValid == 1 && passValid == 1 && mailValid == 1 && cpfValid == 1){
+            console.log("todos validos.")
+        }
+        console.log(mailValid + " / " + cpfValid + " / " + passValid)
+    }
+})()
+
